05ac4b5a61f2f3c7c042447470b51258dd5d8381
sikwan <yysskk99999@naver.com>
2024-01-04 17:18:23 +0900

    first

---
+++ UiAit/UiAit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UiAit
+//
+//  Created by 양시관 on 1/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
